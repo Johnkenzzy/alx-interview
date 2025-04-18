d1613e2e72ec41e6a01f77056b85752c9195810a
johnkenzzy <johnkenumeh@gmail.com>
2025-04-18 05:28:29 +0100

    Pycodestyle compliant

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Defines pascal_triangle function
 """
 
+
 def pascal_triangle(n):
     """
     Returns a list of lists representing the Pascal's triangle of n.
@@ -12,12 +13,12 @@ def pascal_triangle(n):
         return []
 
     triangle = [[1]]  # First row is always [1]
-    
+
     for i in range(1, n):
         row = [1]  # Start each row with 1
         for j in range(1, i):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])  # Sum numbers
         row.append(1)  # End each row with 1
         triangle.append(row)
-    
+
     return triangle
